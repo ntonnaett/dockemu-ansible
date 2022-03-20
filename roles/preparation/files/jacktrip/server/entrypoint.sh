9fee569180414896e1473e295bbe0a3b0038d78a
Nils Tonnätt <nils.tonnaett@posteo.de>
2022-03-20 22:36:58 +0100

    Split container startup and test software execution

--- roles/preparation/files/jacktrip/server/entrypoint.sh
+++ roles/preparation/files/jacktrip/server/entrypoint.sh
@@ -2,10 +2,5 @@
 
 set -e
 
-jackd -P 40 -d dummy --rate ${JACK_SAMPLE_RATE} --period ${JACK_PERIOD} &
-jack_wait -w -t 10 2> /dev/null
+jackd -P 40 -d dummy --rate ${JACK_SAMPLE_RATE} --period ${JACK_PERIOD}
 
-jacktrip -S -t -z --bindport 4464 --nojackportsconnect --broadcast 1024 -q auto
-
-echo "done running jacktrip"
-tail -f /dev/null
